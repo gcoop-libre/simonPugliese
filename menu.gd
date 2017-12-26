b20a367b2b4cda06fb38ca5f50b1a03fe926a2bb
marin-h <marina@gcoop.coop>
2017-12-26 18:19:47 -0300

    Empiezo a hacer el menu del juego

---
+++ menu.gd
@@ -0,0 +1,13 @@
+extends HBoxContainer
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _on_jugar_pressed():
+	get_tree().change_scene("res://principal.tscn")
