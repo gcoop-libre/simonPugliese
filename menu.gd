7fc28f26027f0264cd9896e92e53ee8e59b25ddf
marin-h <marina@gcoop.coop>
2018-02-27 13:44:10 -0300

    Corrijo un typo, es timeout en vez de finish para esperar al timer

--- menu.gd
+++ menu.gd
@@ -6,16 +6,20 @@ func _ready():
 	
 func _on_jugar_pressed():
 	play_click()
+	get_node("Timer").start()
+	yield(get_node("Timer"), "timeout")
 	get_node("/root/global").empezarJuego()
 
 func _on_salir_pressed():
 	play_click()
+	get_node("Timer").start()
+	yield(get_node("Timer"), "timeout")
 	get_tree().quit()
 
 func _on_acerca_pressed():
 	play_click()
 	get_node("Timer").start()
-	yield(get_node("Timer"), "finish")
+	yield(get_node("Timer"), "timeout")
 	get_tree().change_scene("res://info.tscn")
 
 func play_click():
