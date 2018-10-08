93c06567465b0f5f1e99763a85bc608d39cefad0
marin-h <marina@gcoop.coop>
2018-10-08 18:12:07 -0300

    Faltaba instanciar el tileMap

--- tileLaberinto.gd
+++ tileLaberinto.gd
@@ -2,5 +2,5 @@ extends Node2D
 
 func _ready():
 	var nroNivel = get_node("/root/global").subNivelActual
-	var tileMap = load(str("res://TileMap",nroNivel+1,".tscn"))
-	call_deferred('add_child', tileMap)
+	var tileMap = load(str("res://TileMap",nroNivel+1,".tscn")).instance()
+	add_child(tileMap)
