948756f11a599678d6e0229e5e53f70c62bd0e35
marin-h <marina@gcoop.coop>
2018-10-12 11:56:51 -0300

    Acomodo el botón Jugar en el laberinto

--- laberinto.gd
+++ laberinto.gd
@@ -16,7 +16,7 @@ func animarTextoBienvenida():
 	var polygon = Vector2Array([Vector2(164, 264), Vector2(164, 556), Vector2(1100, 556), Vector2(1100, 264)])
 	dialog.set_polygon(polygon)
 	dialog.posicionarTexto(Vector2(185,280))
-	dialog.posicionarBoton(Vector2(870,480))
+	dialog.posicionarBoton(Vector2(820,480))
 	dialog.mostrarTexto(textoBienvenida)
 	dialog.conectarBoton("_on_empezar_pressed", self)
 	add_child(dialog)
