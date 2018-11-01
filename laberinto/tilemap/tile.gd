b53e3a25ed00de2e840c49fe01d4fa6f14c38d6e
marin-h <marina@gcoop.coop>
2018-11-01 13:41:56 -0300

    Elimino recursos que no usabamos

--- laberinto/tilemap/tile.gd
+++
@@ -1,9 +0,0 @@
-extends TileMap
-
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
-
-func posicionarNodoItem(itemNode):
-	get_node("items").add_child(itemNode)
