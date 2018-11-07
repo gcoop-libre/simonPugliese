d2f03b5b7c20ce5cc68696cd013ac754aca39a4b
marin-h <marina@gcoop.coop>
2018-11-07 14:58:24 -0300

    Agrego el msj de ganar al laberinto y hago la animacion del los labels mas suave

--- laberinto/laberinto.gd
+++ laberinto/laberinto.gd
@@ -56,9 +56,9 @@ func agarrarItem(item):
 	get_node("sonidos_laberinto/timerLargo").start()
 	yield(get_node("sonidos_laberinto/timerLargo"), "timeout")
 	if(get_node("tileLaberinto").itemsRestantes() == 0):
-		get_node("ganaste/Panel/anim").play("mostrar")
+		get_node("ganaste/anim").play("mostrar")
 		get_node("sonidos_laberinto").play_sound("win")
-		yield( get_node("ganaste/Panel/anim"), "finished" )
+		yield( get_node("ganaste/anim"), "finished" )
 		emit_signal("gane")
 
 func esJugable():
