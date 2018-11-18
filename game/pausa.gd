b71c7fc66a26eeafb00e9f88919f09672a54e463
Marina Huberman <marina@gcoop.coop>
2018-11-18 19:43:51 -0300

    Tenemos un boton de pausa lindo

--- game/pausa.gd
+++ game/pausa.gd
@@ -1,18 +1,21 @@
-extends Button
+extends TextureButton
+
+var textura_pausado = load("res://assets/buttons/play.png")
+var textura_jugando = load("res://assets/buttons/pause.png")
 
 func _ready():
-	pass
-
+	set_normal_texture(textura_jugando)
+	
 func _on_pausa_pressed():
 	get_tree().set_pause(!(get_tree().is_paused()))
 	if(get_tree().is_paused()):
+		set_normal_texture(textura_pausado)
 		get_node("/root/global").apretar_ui_button()
 		get_node("Timer").start()
 		yield(get_node("Timer"), "timeout")
 		get_node("/root/global").pausarSonido()
-		set_text("continuar")
 	else:
+		set_normal_texture(textura_jugando)
 		get_node("/root/global").continuarSonido()
-		set_text("pausa")
 		get_node("/root/global").apretar_ui_button()
 
